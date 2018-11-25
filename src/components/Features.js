46c00717a027865b06f9736b18c2343894615b13
Mario H. Burgos <mario@petrielabs.com>
2018-11-25 01:47:27 +0000

    Features component is hidden by default.

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Features extends Component {
     render() {
         return (
-            <div class="options-box">
+            <div className="options-box hidden">
                 <h1>Cool Features!</h1>
                 <p>
                     Draw a shape to get the size of the area
