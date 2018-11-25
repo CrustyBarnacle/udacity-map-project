65f8f620d65681a47c007e171cf8940199247a76
Mario H. Burgos <mario@petrielabs.com>
2018-11-25 04:15:59 +0000

    Adds back ServiceWorker.

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 
 class Features extends Component {
-        state = {
+    state = {
         visible: this.visible
-    }
-    
-    handleClick = () => {
-        this.setState = {visible: !this.visible}
     }
     
     render() {
         return (
             <div className="options-box" id="menu">
-                <button onClick={this.handleClick.bind(this)}>
                     Search Neighborhood
                     <h1>Cool Features!</h1>
                     {this.visible &&
                         <input id="toggle-drawing" type="text" placeholder="Enter a Location or Address" />
                     }
-                </button>
             </div>
         );
     }
