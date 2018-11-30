8a97127393cf3c4f982794c5c3c0968ee1b2a528
Mario H. Burgos <mario@petrielabs.com>
2018-11-30 22:55:24 +0000

    Moves arial role/label to <div>. One InfoWindow open at a time.

--- src/components/Container.js
+++ src/components/Container.js
@@ -17,7 +17,7 @@ export class Container extends Component {
         <h1 id="map-heading">Neighborhood Tour</h1>
         <Features visible={true} />
         <Tour />
-        <TourGuide role="application" aria-label="map" />
+        <TourGuide />
       </main>
     );
   }
--- src/components/TourGuide.js
+++ src/components/TourGuide.js
@@ -53,30 +53,45 @@ class TourGuide extends Component {
 
     // Set the parameters for the renderMap() to use
     initMap = () => {
+
+        // Create Map
         const map = new window.google.maps.Map(document.getElementById('map'), {
             center: alumrock,
             zoom: 14,
             style: { style }
         });
-
+        // Set initial map bounds
         let bounds = new window.google.maps.LatLngBounds();
 
+        // Create InfoWindow
+        let infowindow = new window.google.maps.InfoWindow();            
+
+        // Add Markers (bases on FourSquare response)
         this.state.venues.map(foursquareVenue => {
 
+            // Create Marker
             let marker = new window.google.maps.Marker({
                 position: {lat: foursquareVenue.venue.location.lat, lng: foursquareVenue.venue.location.lng},
                 map: map,
                 title: foursquareVenue.venue.name
-              }); 
+              });
+            
+            // Marker clicked
+            marker.addListener('click', function() {
+                infowindow.setContent(foursquareVenue.venue.name)
+            
+                // Open InfoWindow
+                infowindow.open(map, marker);
+            });
 
-              bounds.extend(marker.position);
+            bounds.extend(marker.position); // Add Marker to bounds (to be extended)
         })
-        map.fitBounds(bounds);           
+        map.fitBounds(bounds); // Expand bounds to fit all Markers           
     }
 
     render() {
         return (
-            <div id="map">
+            <div id="map" role="application" aria-label="map">
             </div>
         )
 
