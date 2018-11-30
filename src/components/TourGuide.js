38099962295c4903403170a90943f21675101e45
Mario H. Burgos <mario@petrielabs.com>
2018-11-30 08:39:50 +0000

    Adds axios library for use with FourSquare.

--- src/components/TourGuide.js
+++ src/components/TourGuide.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from 'axios';
+
 
 const style = {
     left: '340px',
@@ -12,12 +14,34 @@ const __GAPI_KEY__ = "AIzaSyB1E9XDW32k70wN-VfijTerghnFWLQk0zY";
 
 class TourGuide extends Component {
     componentDidMount() {
+        this.getVenues()
         this.renderMap()
     }
 
     renderMap = () => {
         scriptLoad("https://maps.googleapis.com/maps/api/js?key=" + __GAPI_KEY__ + "&format=png&style=feature:road|color:0xffffff|visibility:simplified&callback=initMap")
         window.initMap = this.initMap  // specify where to find initMap for the callback function
+    }
+
+    // FourSquare API call with endpoint and parameters
+    getVenues = () => {
+        const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+        let parameters = {
+            query: "tacos",
+            near: "San Jose, CA",
+            client_id: "5OG0BWWMUGOYTPHIGWJS3A3YJSY1FB35UTVBFBS3EUZMFYK1",
+            client_secret: "2SF04EW1UB4AN3QTYDQVAA54GQSRO1FFRHH1DFN5FVBJA5FK",
+            v: "20181128"
+            
+        }
+
+        axios.get(endPoint + new URLSearchParams(parameters))
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log("ERROR! " + error)
+        })
     }
 
     // Set the parameters for the renderMap() to use
