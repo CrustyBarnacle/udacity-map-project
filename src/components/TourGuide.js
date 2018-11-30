0263d0dcae7b2a7244f60346fe1e5346f898870d
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-11-29 16:23:54 -0800

    Cleans up Container.js, styles.css. Getting ready to add FourSquare API.

--- src/components/TourGuide.js
+++ src/components/TourGuide.js
@@ -11,26 +11,25 @@ const __GAPI_KEY__ = "AIzaSyB1E9XDW32k70wN-VfijTerghnFWLQk0zY";
 
 
 class TourGuide extends Component {
+    componentDidMount() {
+        this.renderMap()
+    }
 
     renderMap = () => {
-        // eslint-disable-next-line no-multi-str
-        scriptLoad("https://maps.googleapis.com/maps/api/js?key=" + __GAPI_KEY__ + "&format=png& \
-        style=feature:road|color:0xffffff|visibility:simplified&callback=initMap")
-
+        scriptLoad("https://maps.googleapis.com/maps/api/js?key=" + __GAPI_KEY__ + "&format=png&style=feature:road|color:0xffffff|visibility:simplified&callback=initMap")
         window.initMap = this.initMap  // specify where to find initMap for the callback function
     }
 
     // Set the parameters for the renderMap() to use
     initMap = () => {
         const map = new window.google.maps.Map(document.getElementById('map'), {
-            center: alumrock,  // required parameter
-            style: { style },
-            zoom: 11           // required parameter
+            center: alumrock,
+            zoom: 11,
+            style: { style }
         });
     }
 
     render() {
-        this.renderMap()   // actually draw the map
         return (
             <div id="map">
             </div>
@@ -39,7 +38,7 @@ class TourGuide extends Component {
     }
 }
 
-// =========== insert the <script> code into index.html to allow the javascript to run =========
+
 function scriptLoad(url) {
     let index = window.document.getElementsByTagName('script')[0]
     let script = window.document.createElement('script')
@@ -48,6 +47,6 @@ function scriptLoad(url) {
     script.defer = true
     index.parentNode.insertBefore(script, index)
 }
-// ============ end <script> tag insertion in index.html for js functionality ===============
+
 
 export default TourGuide;
